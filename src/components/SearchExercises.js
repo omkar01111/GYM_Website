58d83e2c27185bfa3eb080335dcc1158c0e16997
omkar <omkarmore0702@gmail.com>
2023-06-08 19:48:11 +0530

    header & searchExercises style changes

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
+import React from "react";
 
 const SearchExercises = () => {
   return (
-    <div>SearchExercises</div>
-  )
-}
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+      <Typography
+        fontWeight="700"
+        mb="50px"
+        textAlign="center"
+        sx={{ fontSize: { lg: "44px", xs: "30px" } }}
+      >
+        Awsome Exercises You <br />
+        should know
+      </Typography>
+      <Box position="relative" mb="72px">
+        <TextField
+          sx={{
+            input: { fontWeight: "bold", border: "none", borderRadius: "4px" },
+            width: { lg: "800px", xs: "350px" },
+            backgroundColor: "white",
+            borderRadius: "40px",
+          }}
+          height="76px"
+          value=""
+          onChange={(e) => {}}
+          placeholder="Search Exercises"
+          type="text"
+        />
+        <Button
+          className="search-btn"
+          sx={{
+            bgcolor: "#ff2625",
+            color: "#fff",
+            textTransform: "none",
+            fontSize: { lg: "20px", xs: "14px" },
+            width: { lg: "175px", xs: "80px" },
+            height: '56px',
+            position: "absolute",
+            right:'0' 
+          }}
+        >
+          Search
+        </Button>
+      </Box>
+    </Stack>
+  );
+};
 
-export default SearchExercises
+export default SearchExercises;
